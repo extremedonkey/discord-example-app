764869a7b9081725134d853a8d071c95097625cb
Reece <extremedonkey@gmail.com>
2024-12-25 00:12:02 +0800

    Auto-commit

---
+++ start-and-push.ps1
@@ -0,0 +1,11 @@
+# Run npm start
+npm run start
+
+# Add all changes to git
+git add .
+
+# Commit changes with a default message
+git commit -m "Auto-commit"
+
+# Push changes to the remote repository
+git push
